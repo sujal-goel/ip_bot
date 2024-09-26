6a70285673cc6289268c77fc7a2c7ea083a2fcc5
sujal goel <sujalgoel19@gmail.com>
2024-09-26 18:20:13 +0530

    homepage ready

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,13 +1,21 @@
 const wrapper = document.querySelector('.wrapper');
-const registerLink= document.querySelector('.register-link');
-const loginLink= document.querySelector('.login-link');
-
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+const logregLinks = document.querySelectorAll('.logreg-link');
 
 registerLink.addEventListener("click", () => {
+    logregLinks[0].style.display = 'none';
+    setTimeout(() => {
+        logregLinks[1].style.display = 'block';
+    }, 700);
     wrapper.classList.add('active');
-
 });
-loginLink.addEventListener("click",() => {
+
+loginLink.addEventListener("click", () => {
+    logregLinks[0].style.display = 'none';
     wrapper.classList.remove('active');
-}
-);
+    
+    setTimeout(() => {
+        logregLinks[0].style.display = 'block'; 
+    }, 700);
+});
